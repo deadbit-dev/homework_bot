470882b7edae4f629a601fe0e81728a6733cd574
Pavel <pavelpetrochenko2001@gmail.com>
2021-10-14 02:22:16 +0300

    Update homework.py
    add env variable and implementation get_api_answer, check_response, parse_status

--- homework.py
+++ homework.py
@@ -1,12 +1,21 @@
+import requests
 import logging
+import time
+import os
 
-...
+import telegram
+from dotenv import load_dotenv
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-CHAT_ID = ...
+load_dotenv()
 
-...
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
+
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
 
 RETRY_TIME = 300
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -17,38 +26,51 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена, в ней нашлись ошибки.'
 }
 
-
+#TODO: send notification to telegram account
 def send_message(bot, message):
-    ...
+    """ """
+    pass
 
 
 def get_api_answer(url, current_timestamp):
-    ...
+    """Request the PRACTICUM API and get response 
+    between current_timestamp and current time"""
+
+    url = ENDPOINT
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+    return requests.get(url, headers=headers, params=payload).json()
 
 
 def parse_status(homework):
-    verdict = ...
-    ...
+    """Return the message by homework status"""
 
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    verdict = HOMEWORK_STATUSES.get(homework.get('status'))
+    homework_name = homework.get('homework_name')
+    return f'Изменился статус проверки работы {homework_name}. {verdict}'
 
 
 def check_response(response):
+    """Return true if changed hamework status"""
+
     homeworks = response.get('homeworks')
-    ...
+    return len(homeworks) > 0
 
 
+#TODO: exception and logging module
 def main():
+    """ """
+    
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    ...
+    pass
     while True:
         try:
-            ...
+            pass
             time.sleep(RETRY_TIME)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            pass
             time.sleep(RETRY_TIME)
             continue
 
